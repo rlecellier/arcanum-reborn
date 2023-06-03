4744560a26a3ec421e18658bac77af15354af466
Romain Le Cellier <rlecellier@gmail.com>
2023-06-03 15:40:07 +0200

    Add .circleci/config.yml

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
-import styles from './App.module.scss';
-import logo from './logo.svg';
+import styles from './App.module.scss'
+import logo from './logo.svg'
 
 function App(): JSX.Element {
   return (
