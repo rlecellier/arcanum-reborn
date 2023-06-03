e472bc02aa249887bda35515551cf44c86ccfb23
Romain Le Cellier <rlecellier@gmail.com>
2023-06-03 15:40:07 +0200

    Add .circleci/config.yml

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 
-import './index.scss';
+import './index.scss'
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
