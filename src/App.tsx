bfb9699201a6dcd91386579206f4922b949d51ee
Romain Le Cellier <rlecellier@gmail.com>
2023-06-03 12:50:25 +0200

    🔨(initialize) react-create-app
    Pull and configure react-create-app to have:
    
    * sass
    * css-modules
    * react-testing-library

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import styles from './App.module.scss';
+import logo from './logo.svg';
+
+function App(): JSX.Element {
+  return (
+    <div className={styles.App}>
+      <header className={styles['App-header']}>
+        <img src={logo} className={styles['App-logo']} alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className={styles['App-link']}
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
